4d626ba565e45e37d79decb4b7c2b26e936d9d64
ben.pplau <ben.pplau@lalamove.com>
2023-10-02 19:52:54 +0800

    update highlight

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,12 @@
 return { -- this table overrides highlights in all themes
   -- Normal = { bg = "#000000" },
+  LspReferenceText = {
+    bg = "#997FFB"
+  },
+  LspReferenceRead = {
+    bg = "#997FFB"
+  },
+  LspReferenceWrite = {
+    bg = "#997FFB"
+  }
 }
