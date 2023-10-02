1049f0a7ffb28e98211d30140d72b2e25572acd6
ben.pplau <ben.pplau@lalamove.com>
2023-10-02 13:35:19 +0800

    update plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,27 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+      -- You can also add new plugins here as well:
+      -- Add plugins, the lazy syntax
+      -- "andweeb/presence.nvim",
+      -- {
+      --   "ray-x/lsp_signature.nvim",
+      --   event = "BufRead",
+      --   config = function()
+      --     require("lsp_signature").setup()
+      --   end,
+      -- },
+      {
+            "olivercederborg/poimandres.nvim",
+            name = "poimandres",
+            config = function()
+                  require("poimandres").setup()
+            end,
+      },
+      {
+            "nvim-lualine/lualine.nvim",
+            name = "lualine",
+            dependencies = {"nvim-tree/nvim-web-devicons"},
+            config = function()
+                  require("lualine").setup({})
+            end,
+      }
 }
