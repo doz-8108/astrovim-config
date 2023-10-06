be539c3c4789b139cf1d893fa0f82b598cebf3cc
ben.pplau <ben.pplau@lalamove.com>
2023-10-06 18:24:34 +0800

    update plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -24,5 +24,44 @@ return {
             config = function()
                   require("lualine").setup({})
             end,
+      },
+      -- {
+      --       'akinsho/git-conflict.nvim',
+      --       name = "git-conflict-resolver",
+      --       lazy = false,
+      --       config = function ()
+      --             require("git-conflict").setup({})
+      --       end
+      -- },{
+      "jay-babu/mason-null-ls.nvim",
+      opts = {
+            handlers = {
+                  -- for prettierd
+                  prettierd = function()
+                        require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
+                              condition = function(utils)
+                                    return utils.root_has_file "package.json"
+                                          or utils.root_has_file ".prettierrc"
+                                          or utils.root_has_file ".prettierrc.json"
+                                          or utils.root_has_file ".prettierrc.js"
+                              end,
+                        })
+                  end,
+                  -- For eslint_d:
+                  eslint_d = function()
+                        require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
+                              condition = function(utils)
+                                    return utils.root_has_file "package.json"
+                                          or utils.root_has_file ".eslintrc.json"
+                                          or utils.root_has_file ".eslintrc.js"
+                              end,
+                        })
+                  end,
+            },
+      },
+      {
+            "tpope/vim-surround",
+            name = "vim-surround",
+            lazy = false,
       }
 }
