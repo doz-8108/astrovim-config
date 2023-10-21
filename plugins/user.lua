84a2757962dfb92eeed24fd677905a1804f35541
pakpannn <ben.pakpannn@gmail.com>
2023-10-21 23:56:24 +0800

    formatting

--- plugins/user.lua
+++ plugins/user.lua
@@ -96,5 +96,15 @@ return {
       },
       {
             "sindrets/diffview.nvim"
+      },
+      {
+            "nvim-neo-tree/neo-tree.nvim",
+            opts = function(_, opts)
+                  opts.filesystem.filtered_items = {
+                        visible = true,
+                        hide_dotfiles = false,
+                        hide_gitignored = true
+                  }
+            end,
       }
 }
